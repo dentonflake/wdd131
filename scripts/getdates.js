dcd79f89f6d65389c4d51b07bea4a26d02ed9594
Denton Flake <me@dentonflake.com>
2025-05-12 12:56:40 -0700

    Completed bom and media activities

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,4 +3,4 @@ const yearElement = document.getElementById('currentyear');
 yearElement.textContent = new Date().getFullYear().toString();
 
 const lastModified = document.getElementById('lastModified');
-lastModified.textContent = `Last Modified: ${document.lastModified}`
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
